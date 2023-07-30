373d1a8ff3642a3a11464f36908cdb0690b9de65
gusujin <gusu@likelion.net>
2023-07-30 17:37:58 +0900

    feat: 칸반보드 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  CheckCircleIcon,
+  PlayCircleIcon,
+  PlusIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-full bg">
+      <main className="p-5 bg-slate-400 rounded">
+        <h2 className="text-white font-bold text-2xl mb-5">Project Kanban</h2>
+        <div className="flex gap-x-5 items-start">
+          <aside className="p-3 rounded w-80 bg-neutral-200">
+            <h3 className="font-bold text-lg mb-2">To Do</h3>
+            <ul className="flex flex-col gap-y-2">
+              <li className="bg-white rounded p-2">
+                <div className="flex justify-between items-center">
+                  <div className="flex gap-x-1 items-center">
+                    <CheckCircleIcon className="w-4 h-auto" />
+                    <p className="text-base font-semibold">New Task</p>
+                  </div>
+                  <div className="w-4">
+                    <XMarkIcon />
+                  </div>
+                </div>
+                <div className="flex justify-end">
+                  <PlayCircleIcon className="w-4 h-auto" />
+                </div>
+              </li>
+              <li className="bg-white rounded p-2">
+                <div className="flex justify-between items-center">
+                  <div className="flex gap-x-1 items-center">
+                    <CheckCircleIcon className="w-4 h-auto" />
+                    <p className="text-base font-semibold">New Task</p>
+                  </div>
+                  <div className="w-4">
+                    <XMarkIcon />
+                  </div>
+                </div>
+                <div className="flex justify-end">
+                  <PlayCircleIcon className="w-4 h-auto" />
+                </div>
+              </li>
+            </ul>
+            <div className="flex items-center mt-4">
+              <PlusIcon className="w-5 h-auto" />
+              <p>Add a card</p>
+            </div>
+          </aside>
+          <aside className="p-3 rounded w-80 bg-neutral-200">
+            <h3 className="font-bold text-lg mb-2">In Progress</h3>
+            <div className="flex items-center mt-4">
+              <PlusIcon className="w-5 h-auto" />
+              <p>Add a card</p>
+            </div>
+          </aside>
+          <aside className="p-3 rounded w-80 bg-neutral-200">
+            <h3 className="font-bold text-lg mb-2">Done</h3>
+            <div className="flex items-center mt-4">
+              <PlusIcon className="w-5 h-auto" />
+              <p>Add a card</p>
+            </div>
+          </aside>
+        </div>
+      </main>
     </div>
   );
 }
